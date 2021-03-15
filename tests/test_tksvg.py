91c56badf8db4ec3b4e466f63f5336964f9f1162
RedFantom <redfantom@outlook.com>
2021-03-15 11:13:07 +0100

    Update path finding in tests

--- tests/test_tksvg.py
+++ tests/test_tksvg.py
@@ -6,20 +6,22 @@ Copyright (c) 2021 RedFantom
 from unittest import TestCase
 import tkinter as tk
 import tksvg
+import os
 
 
 class TestTkSVG(TestCase):
     def setUp(self):
         self.window = tk.Tk()
+        self._path = os.path.abspath(os.path.dirname(__file__))
 
     def test_tksvg_load(self):
         tksvg.load(self.window)
         # Create test PhotoImage
-        tk.PhotoImage(file="tests/orb.svg")
+        tk.PhotoImage(file=os.path.join(self._path, "orb.svg"))
 
     def test_svg_image(self):
         tksvg.load(self.window)
-        image = tksvg.SvgImage(file="tests/orb.svg", scale=0.5)
+        image = tksvg.SvgImage(file=os.path.join(self._path, "orb.svg"), scale=0.5)
         self.assertEqual(image.cget("scale"), 0.5)
         self.assertEqual(image.cget("scale"), image["scale"])
 
